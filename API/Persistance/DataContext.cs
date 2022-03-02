a7f460d0acd53230f2e896ad302cf4fcbc8ab082
Schullert Gabriel <schullergabriel13@gmail.com>
2022-03-02 15:31:56 +0200

    Add project files.

---
+++ API/Persistance/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using API.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Persistance
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Activity> Activities { get; set; }
+    }
+}
