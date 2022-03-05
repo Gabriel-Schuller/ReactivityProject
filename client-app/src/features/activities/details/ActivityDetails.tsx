f2b66975f28a93203f00ec28dda1d3432e2a2c5f
Schullert Gabriel <schullergabriel13@gmail.com>
2022-03-05 23:46:37 +0200

    Implement React Routing

--- client-app/src/features/activities/details/ActivityDetails.tsx
+++ client-app/src/features/activities/details/ActivityDetails.tsx
@@ -1,17 +1,24 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Button, Card, Image} from "semantic-ui-react";
 import {useStore} from "../../../app/stores/store";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import {Link, useParams} from "react-router-dom";
+import {observer} from "mobx-react-lite";
 
 
 
 function ActivityDetails() {
 
     const {activityStore} = useStore();
+    const {selectedActivity: activity, loadActivity, loadingInitial}= activityStore;
+    const {id} = useParams<{id: string}>();
 
-    const {selectedActivity: activity, openForm, cancelSelectedActivity}= activityStore;
+    useEffect(() => {
+        if (id) loadActivity(id);
+    }, [id, loadActivity]);
 
-    if (!activity) return <LoadingComponent/>;
+
+    if (loadingInitial || !activity) return <LoadingComponent/>;
 
     return (
         <Card fluid>
@@ -27,12 +34,12 @@ function ActivityDetails() {
             </Card.Content>
             <Card.Content extra>
                 <Button.Group widths={2}>
-                    <Button onClick={() => openForm(activity.id)} basic color="blue" content="Edit"/>
-                    <Button onClick={cancelSelectedActivity} basic color="grey" content="Cancel"/>
+                    <Button as={Link} to={`/manage/${activity.id}`} basic color="blue" content="Edit"/>
+                    <Button as={Link} to="/activities" basic color="grey" content="Cancel"/>
                 </Button.Group>
             </Card.Content>
         </Card>
     );
 }
 
-export default ActivityDetails;
+export default observer(ActivityDetails);
